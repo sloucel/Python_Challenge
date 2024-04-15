152752ffbe9bd5596c24c9a90232a209320c9fcd
sloucel <164970856+sloucel@users.noreply.github.com>
2024-04-14 18:29:17 -0700

    Update main.py
    Original code to pull budget_data.csv

--- Starter_Code/PyBank/main.py
+++ Starter_Code/PyBank/main.py
@@ -1,2 +1,13 @@
-# Testing github terminal
-Print("Hello World")
+# Import os module
+import os
+
+# Import csv module
+import csv
+
+# Create csvpath
+csvpath = os.path.join('..', 'Resources','budget_data.csv')
+
+# Use the def block 
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter= ',')
+    next(csvreader)
