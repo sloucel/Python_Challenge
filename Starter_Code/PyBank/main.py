98309d8a5cd8058b3aab2365f59b6b99f59eead2
sloucel <164970856+sloucel@users.noreply.github.com>
2024-04-14 18:31:33 -0700

    Update main.py
    Added code to get total month count by add a 'def' and a new command under 'with open'

--- Starter_Code/PyBank/main.py
+++ Starter_Code/PyBank/main.py
@@ -7,7 +7,15 @@ import csv
 # Create csvpath
 csvpath = os.path.join('..', 'Resources','budget_data.csv')
 
+# Create block to calculate the the total number of months
+def total_months(csvreader):
+    month_count = 0
+    for row_lp in csvreader:
+        month_count += 1
+    return month_count
+    
 # Use the def block 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter= ',')
     next(csvreader)
+    total_months_count = total_months(csvreader)
