8d5754bfd2ea38e82d27e67126177906b8de9549
sloucel <164970856+sloucel@users.noreply.github.com>
2024-04-14 21:08:12 -0700

    Update main.py
    Added 'def block',  'with open', and print for NET PROFIT

--- Starter_Code/PyBank/main.py
+++ Starter_Code/PyBank/main.py
@@ -1,21 +1,43 @@
 # Import os module
 import os
-
-# Import csv module
 import csv
 
 # Create csvpath
 csvpath = os.path.join('..', 'Resources','budget_data.csv')
 
-# Create block to calculate the the total number of months
+# Create block for TOTAL MONTHS
 def total_months(csvreader):
     month_count = 0
     for row_lp in csvreader:
         month_count += 1
     return month_count
+
+# Create block for NET PROFIT
+def net_profitloss(csvreader):
+    profit = 0
+    loss = 0 
+    for row_lp in csvreader:
+        value = int(row_lp[1])
+        if value > 0:
+            profit += value
+        else:
+            loss += value
+    return profit, loss
     
 # Use the def block 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter= ',')
     next(csvreader)
     total_months_count = total_months(csvreader)
+
+    csvfile.seek(0)
+    next(csvreader)
+    total_profit, total_loss = net_profitloss(csvreader)
+    net_total = total_profit + total_loss
+
+# PRINT STATION
+print(f'Total Months: {total_months_count}')
+# print(f'Total Profit: {total_profit}')
+# print(f'Total Loss: {total_loss}')
+print(f'Net Total: ${net_total}')
+
